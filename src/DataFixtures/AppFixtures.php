3792b224b0b4bce1121ee29ba42572998411fbaf
hugolgs-dev <hugo.ligneres@orange.fr>
2024-12-17 00:11:31 +0100

    init

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        /* Création d'un premier article*/
+        $article = new Article();
+        $article->setTitre("Premier article");
+        $article->setTexte("Lorem ipsum");
+        $manager->persist($article);
+
+        /* Création d'un second article*/
+        $article = new Article();
+        $article->setTitre("Second article");
+        $article->setTexte("Lorem ipsum");
+        $manager->persist($article);
+
+        $manager->flush();
+    }
+}
